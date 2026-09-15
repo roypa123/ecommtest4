9152c30dea05a5f97a99ef21c3482fe8e51aac4b
roypa123 <roypa81130@gmail.com>
2026-09-15 21:19:11 +0530

    fix: some changes

---
+++ src/main/java/com/ram/ecommerce/repository/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.ram.ecommerce.repository;
+
+import com.ram.ecommerce.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository {
+}
