492bbe656cd9c7e4bb7d84c0152ae2e1f010570f
roypa123 <roypa81130@gmail.com>
2026-09-15 20:32:14 +0530

    fix: some changes

---
+++ src/main/resources/db/migration/V2__create_categories_and_sub_categories.sql
@@ -0,0 +1,15 @@
+CREATE TABLE categories(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL UNIQUE,
+    image_url VARCHAR(500),
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE subcategories (
+    id BIGSERIAL PRIMARY KEY,
+    category_id BIGINT NOT NULL REFERENCES categories(id)
+    name VARCHAR(150) NOT NULL,
+    image_url VARCHAR(500),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE(category_id, name)
+)
---
+++ src/main/resources/db/migration/V3__create_products.sql
@@ -0,0 +1,10 @@
+CREATE TABLE products (
+    id BIGSERIAL PRIMARY KEY,
+    subcategory_id BIGINT NOT NULL REFERENCES subcategories(id) ON DELETE CASCADE,
+    name VARCHAR(150) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    image_url VARCHAR(500),
+    price NUMERIC(10,2) NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+)
