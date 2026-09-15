52ff96e2441801b8ac2f52fdb44b2686fc83c486
roypa123 <roypa81130@gmail.com>
2026-09-15 22:30:17 +0530

    fix: some changes

--- src/main/java/com/ram/ecommerce/security/JwtAuthFilter.java
+++ src/main/java/com/ram/ecommerce/security/JwtAuthFilter.java
@@ -5,7 +5,46 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 
-public class JwtAuthFilter {
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse respoane,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+
+            if(jwtService.isTokenValid|(token)){
+                String email = jwtService.extractEmail(token);
+
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        email, null, List.of()
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
