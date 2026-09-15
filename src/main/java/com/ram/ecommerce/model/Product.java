e8bdc7789b2806bae2a15145b95d6050291c5d0e
roypa123 <roypa81130@gmail.com>
2026-09-15 21:11:55 +0530

    fix: some changes

---
+++ src/main/java/com/ram/ecommerce/model/Product.java
@@ -0,0 +1,41 @@
+package com.ram.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="subctegory_id", nullable = false)
+    private SubCategory subCategory;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+
+    @Column(nullable = false, precision =10, scale =2)
+    private BigDecimal price;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+}
