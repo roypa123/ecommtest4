bad9dd6b637f9805f1f61630e69098f5d938ce84
roypa123 <roypa81130@gmail.com>
2026-09-16 20:35:46 +0530

    fix: some changes

---
+++ src/main/java/com/ram/ecommerce/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.ram.ecommerce.controller;
+
+import com.ram.ecommerce.service.ProductService;
+import com.ram.ecommerce.view.ProductResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<ProductResponse> create(
+            @PathVariable Long subCategoryId,
+            @RequestParam("name") String name,
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("image") MultipartFile image
+    ){
+        return ResponseEntity.ok(productService.create(subCategoryId, name, title, description, price, image));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAll(@PathVariable Long subCategoryId){
+        return ResponseEntity.ok(productService.getBySubCategory(subCategoryId));
+    }
+
+}
