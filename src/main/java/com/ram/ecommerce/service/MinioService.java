39979dcba020a2c814b609b325ca642c31bebde5
roypa123 <roypa81130@gmail.com>
2026-09-16 11:28:38 +0530

    fix: some changes

---
+++ src/main/java/com/ram/ecommerce/service/MinioService.java
@@ -0,0 +1,25 @@
+package com.ram.ecommerce.service;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class MinioService {
+
+
+
+
+
+
+
+
+}
