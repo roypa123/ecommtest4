6a10074fa410807e828468cacc66f19cf0098d73
roypa123 <roypa81130@gmail.com>
2026-09-16 20:56:20 +0530

    fix: some changes

--- src/main/java/com/ram/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ram/ecommerce/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
+                                "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
                         ).permitAll()
